27ca7b87d6bb283d536db2f1801769bdd17422f3
Sebastian Guenther <sebastian.guenther@ifes.uni-hannover.de>
2024-04-17 18:58:27 +0200

    custom +features calculation in chunks, works too slow
    Feature calculation is done in 8 chunks of manifold ts to stay within
    RAM restrictions. However, feature calculation for a single ts with 8760
    points needs ~4s instead of ~0.15s for a ts with 1000 points (summing up
    to 25 days instead of 10-15 hours for 500 000 ts). Following,
    calculation must be moved to cluster.
    
    On branch feature/custom
    Changes to be committed:
          modified:   examples/redispatch/features_rd.py

--- examples/redispatch/features_rd.py
+++ examples/redispatch/features_rd.py
@@ -1,1 +1,32 @@
 #!/usr/bin/env python3
+import pandas as pd
+
+import estss
+
+
+MANIFOLD_PATHS = tuple(
+    f'examples/redispatch/ts_manifold{ii}.pkl' for ii in range(1, 9)
+)
+FEAT_PATHS = tuple(s.replace('ts', 'feat') for s in MANIFOLD_PATHS)
+FPATH = 'examples/redispatch/feat_manifold.pkl'
+
+
+def get_features(path=FPATH):
+    return pd.read_pickle(path)
+
+
+def compute_features(mani_paths=MANIFOLD_PATHS, feat_paths=FEAT_PATHS):
+    for ii, (mpath, fpath) in enumerate(zip(mani_paths, feat_paths)):
+        print(f'Computing Feature Set #{ii+1}/{len(mani_paths)}')
+        df_ts = pd.read_pickle(mpath)
+        df_feat = estss.features.features_for_df(df_ts)
+        df_feat.to_pickle(fpath)
+    return None
+
+
+def merge_features(feat_paths=FEAT_PATHS, save=False):
+    df_list = [pd.read_pickle(fpath) for fpath in feat_paths]
+    df_feat = pd.concat(df_list, axis=0, ignore_index=True)
+    if save:
+        df_feat.to_pickle(save)
+    return df_feat
